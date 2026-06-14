195b51e5ef1bcdc5f242c6573149eaca9ec468b4
SeigoTakahashi <seigo2004takahashi@gmail.com>
2026-06-14 15:40:54 +0900

    UserProfileContextを削除

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import GoalsPage from "./pages/GoalsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { AuthContextProvider } from "../AuthContext";
 import { MyLibraryContextProvider } from "../MyLibraryContext";
-import { UserProfileContextProvider } from "../UserProfileContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -35,73 +34,71 @@ function App() {
     <>
       <AuthContextProvider>
         <MyLibraryContextProvider>
-          <UserProfileContextProvider>
-            <BrowserRouter>
-              <ScrollToTop />
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <ProtectedRoute>
-                      <DashboardPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/mylibrary"
-                  element={
-                    <ProtectedRoute>
-                      <MyLibraryPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/mylibrary/input"
-                  element={
-                    <ProtectedRoute>
-                      <BookInfoInputPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/mylibrary/edit/:id"
-                  element={
-                    <ProtectedRoute>
-                      <BookInfoEditPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/mylibrary/detail/:id"
-                  element={
-                    <ProtectedRoute>
-                      <BookInfoDetailPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/analysis"
-                  element={
-                    <ProtectedRoute>
-                      <AnalysisPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/goals"
-                  element={
-                    <ProtectedRoute>
-                      <GoalsPage />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                {/* catch-all: 存在しないルートは NotFoundPage を表示 */}
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </BrowserRouter>
-          </UserProfileContextProvider>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <DashboardPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mylibrary"
+                element={
+                  <ProtectedRoute>
+                    <MyLibraryPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mylibrary/input"
+                element={
+                  <ProtectedRoute>
+                    <BookInfoInputPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mylibrary/edit/:id"
+                element={
+                  <ProtectedRoute>
+                    <BookInfoEditPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/mylibrary/detail/:id"
+                element={
+                  <ProtectedRoute>
+                    <BookInfoDetailPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/analysis"
+                element={
+                  <ProtectedRoute>
+                    <AnalysisPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/goals"
+                element={
+                  <ProtectedRoute>
+                    <GoalsPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              {/* catch-all: 存在しないルートは NotFoundPage を表示 */}
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </BrowserRouter>
         </MyLibraryContextProvider>
       </AuthContextProvider>
     </>
